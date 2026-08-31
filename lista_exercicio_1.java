7b81220553cde08ddd6218ea51d121eb9534f61b
Robério Dias Argollo Neto <roberiodiasofc@gmail.com>
2026-08-31 00:48:23 -0300

    Atualizar o lista_exercicio_1.java

--- lista_exercicio_1.java
+++ lista_exercicio_1.java
@@ -4,17 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+      Locale.setDefault(Locale.US);
+      Scanner sc = new Scanner (System.in);
+
       int a;
       int b;
       
         System.out.println("Escreva  valor inteiro: ");
-        a = sc.next.Int();
+        a = sc.nextInt();
 
         System.out.println("Escreva outro valor inteiro: ");
-        b = sc.next.Int();
+        b = sc.nextInt();
 
         int c = a + b;
 
         System.out.println("SOMA = " + c );
-}
+  }
 }        
