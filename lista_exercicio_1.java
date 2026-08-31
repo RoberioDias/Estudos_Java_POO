601782c30b1254feaf6eb2f2276e4a64fe071813
Robério Dias Argollo Neto <roberiodiasofc@gmail.com>
2026-08-31 00:46:15 -0300

    Criar lista_exercicio_1.java

---
+++ lista_exercicio_1.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+      int a;
+      int b;
+      
+        System.out.println("Escreva  valor inteiro: ");
+        a = sc.next.Int();
+
+        System.out.println("Escreva outro valor inteiro: ");
+        b = sc.next.Int();
+
+        int c = a + b;
+
+        System.out.println("SOMA = " + c );
+}
+}        
